fa2a711982e20774db84a99de5bb8e47feab0341
Ankesh-21 <hatuiankesh2018@gmail.com>
2025-09-06 13:58:03 +0530

    Login-Signup

---
+++ Login-Signup/loginSignup/base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from django.contrib.auth import urls
+from . import views
+from django.contrib.auth.views import LogoutView
+urlpatterns = [
+    path('',views.Home,name='home'),
+    path('signup/',views.authView,name = 'signup'),
+    path('accounts/logout/',LogoutView.as_view(),name='logout'),
+    path("accounts/", include("django.contrib.auth.urls")),
+]
